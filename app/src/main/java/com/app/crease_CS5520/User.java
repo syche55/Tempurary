79e979eb29e904a14168263bd46d1e5ace472d79
siyuchen2020 <chen.siyu2@husky.neu.edu>
2020-07-02 19:10:10 -0700

    Test test again for Jing

--- app/src/main/java/com/app/crease_CS5520/User.java
+++ app/src/main/java/com/app/crease_CS5520/User.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class User {
     public String username;
-    public ArrayList<Integer> stickers;
-    public ArrayList<Integer> history;
+    public ArrayList<String> stickers;
+    public ArrayList<String> history;
 
     public User(){
     }
@@ -13,7 +13,8 @@ public class User {
     public User(String username){
         this.username = username;
         this.stickers = new ArrayList<>();
-        stickers.add(0);
+        stickers.add(new String(Character.toChars(0x2764)));
+
     }
 
 }
