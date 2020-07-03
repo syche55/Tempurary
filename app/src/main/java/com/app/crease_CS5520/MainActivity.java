7368f24866ad563f2d0fea7a29a6bd17cf9881d7
jshen1110 <jshen1110@users.noreply.github.com>
2020-07-02 21:49:18 -0700

    Update Layout

--- app/src/main/java/com/app/crease_CS5520/MainActivity.java
+++ app/src/main/java/com/app/crease_CS5520/MainActivity.java
@@ -1,20 +1,14 @@
 package com.app.crease_CS5520;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,20 +18,163 @@ import com.google.firebase.database.MutableData;
 import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
-import com.app.crease_CS5520.data.model.LoggedInUser;
-
-
-
-
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DatabaseReference mDatabase;
+    private TextView curUser;
+    private TextView otherUser;
+    private TextView curSticker;
+    private TextView otherSticker;
+    private Button sendSticker;
+    private Button getHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // init users' name
+        curUser = (TextView) findViewById(R.id.curUser);
+        otherUser = (TextView) findViewById(R.id.otherUser);
+        // init sticker view
+        curSticker = (TextView)findViewById(R.id.curSticker);
+        otherSticker = (TextView) findViewById(R.id.otherSticker);
+        // init database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        sendSticker = (Button)findViewById(R.id.sendSticker);
+        sendSticker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //MainActivity.this.onsendSticker(mDatabase, curUser.isChecked() ? "user1" : "user2");
+            }
+        });
+
+        getHistory = (Button)findViewById(R.id.getHistory);
+        getHistory.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                getHistory();
+            }
+        });
+
+
+
+        mDatabase.child("users").addChildEventListener(
+                new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        User user = dataSnapshot.getValue(User.class);
+
+                        if (dataSnapshot.getKey().equalsIgnoreCase("user1")) {
+                            score.setText(user.score);
+                            userName.setText(user.username);
+                        } else {
+                            score2.setText(String.valueOf(user.score));
+                            userName2.setText(user.username);
+                        }
+                        Log.e(TAG, "onChildAdded: dataSnapshot = " + dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        User user = dataSnapshot.getValue(User.class);
+
+                        if (dataSnapshot.getKey().equalsIgnoreCase("user1")) {
+                            score.setText(user.score);
+                            userName.setText(user.username);
+                        } else {
+                            score2.setText(String.valueOf(user.score));
+                            userName2.setText(user.username);
+                        }
+                        Log.v(TAG, "onChildChanged: "+dataSnapshot.getValue().toString());
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled:" + databaseError);
+                    }
+                }
+        );
+    }
+
+    public void getHistory(DatabaseReference postRef, String user) {
+        startActivity(new Intent(MainActivity.this, ShowHistory.class));
+    }
+
+    /**
+     * Called on score add
+     * @param postRef
+     * @param user
+     */
+    private void onAddScore(DatabaseReference postRef, String user) {
+        postRef
+                .child("users")
+                .child(user)
+                .runTransaction(new Transaction.Handler() {
+                    @Override
+                    public Transaction.Result doTransaction(MutableData mutableData) {
+                        User u = mutableData.getValue(User.class);
+                        if (u == null) {
+                            return Transaction.success(mutableData);
+                        }
+
+                        u.score = String.valueOf(Integer.valueOf(u.score) + 5);
+
+                        mutableData.setValue(u);
+                        return Transaction.success(mutableData);
+                    }
+
+                    @Override
+                    public void onComplete(DatabaseError databaseError, boolean b,
+                                           DataSnapshot dataSnapshot) {
+                        // Transaction completed
+                        Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+                    }
+                });
+    }
+
+
+    public void doAddDataToDb(View view){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database by listening for a change to that item.
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                TextView tv = (TextView)findViewById(R.id.dataUpdateTextView);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
     }
+
+
 
 }
