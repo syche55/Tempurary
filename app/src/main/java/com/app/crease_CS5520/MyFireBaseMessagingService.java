db45c002ff7339912a72faed3dfc372c3c8b6723
siyuchen2020 <chen.siyu2@husky.neu.edu>
2020-07-07 12:30:36 -0700

    - Notification shows when app in background - Notification display - Code clean up

--- app/src/main/java/com/app/crease_CS5520/MyFireBaseMessagingService.java
+++ app/src/main/java/com/app/crease_CS5520/MyFireBaseMessagingService.java
@@ -13,16 +13,11 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
 
         }
         // Check if message contains a notification payload.
@@ -30,19 +25,11 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 
-
+    // new token created and recorded here
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "Refreshed token: " + token);
-
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-//        sendRegistrationToServer(token);
     }
 }
